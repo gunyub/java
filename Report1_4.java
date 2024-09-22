6f36d76cf7fb51291fe25e5967a72c17874fcbc9
gunyub <gunyub1120@gmail.com>
2024-09-22 21:42:50 +0900

    Update Report1_4.java
    code error

--- Report1_4.java
+++ Report1_4.java
@@ -7,8 +7,9 @@ public class Report1_4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("$1=1200원입니다. 달러를 입력하세요>> ");
-		int score = scanner.nextInt();
-		System.out.println("$253는" + score*1200 +"입니다");
+		int money = scanner.nextInt();
+		int total = money*1200;
+		System.out.println("$"+money+"는 " + total +"입니다");
 		
 		scanner.close();
 	}
